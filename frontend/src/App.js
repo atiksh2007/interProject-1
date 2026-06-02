3024079c2689dbcd7735c8a76d19c850f58da064
atiksh2007 <preeti.mishra2509@gmail.com>
2026-06-02 16:41:41 +0530

    done

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+function App() {
+
+ return (
+  <div>
+
+   <Signup />
+
+   <hr />
+
+   <Login />
+
+  </div>
+ );
+}
+
+export default App;
